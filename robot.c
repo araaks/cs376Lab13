c7a32c6f20c9834ad88add79d3c61aa9a85e0fe5
campuzan21 <campuzan21@up.edu>
2018-12-06 17:18:29 -0800

    Checkpoint5 commit nico

--- robot.c
+++ robot.c
@@ -35,7 +35,7 @@ void robotPrintAscii(void)
   blank = blank +30;
   printf("\n\n"
 	 "UWR: Unidentified Wheeled Robot\n"
-	 "%s    i_i    \n"
+	 "%s    0_0    \n"
 	 "%s   [~_~]   \n"
 	 "%s  /|___|\\ \n"
 	 "%s   d   b   \n",blank, blank, blank, blank);
